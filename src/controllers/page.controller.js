8c40382760304520856b1bc0aad04f794269f907
Kambaia Alberto <kambaiaalberto@gmail.com>
2023-11-06 10:58:30 +0100

    new files fixed bug in new page

--- src/controllers/page.controller.js
+++ src/controllers/page.controller.js
@@ -7,14 +7,17 @@ module.exports = {
             mensagem: "Bem-vindo ao EJS!",
             path: req.url,
         };
+
         const news4 = news.map(objeto => ({
             ...objeto
         })).slice(0, 4);
         const news3 = news.map(objeto => ({
             ...objeto
         })).slice(0, 3);
+
         res.render("pt/index", { data, smollNews: news4, fullNews: news, news3 });
     },
+
     async aboutPage(req, res) {
         const data = {
             titulo: "Grupo Boavida - Grupo empresarial: Inovação e gestão por Angola",
@@ -40,20 +43,17 @@ module.exports = {
         })).slice(0, 3);
         res.render("pt/news", { data, smollNews: news4, fullNews: news, news3 });
     },
-
     newPage(req, res) {
         const data = {
             titulo: "Grupo Boavida - Grupo empresarial: Inovação e gestão por Angola",
             mensagem: "Bem-vindo ao EJS!",
             path: req.url,
         };
-        const featured = news.filter((ietmNew) => ietmNew.id === parseInt(req.params.id)).map((ietmNew) => ietmNew);
-
+        const featured = news[parseInt(req.params.id) - 1];
         const news3 = news.map(objeto => ({
             ...objeto
         })).slice(0, 3);
-
-        res.render("pt/new", { data, pathName: parseInt(req.params.id), featured: featured[0], news, news3 });
+        res.render("pt/new", { data, pathName: parseInt(req.params.id), featured, news, news3 });
     },
     async videoPage(req, res) {
         const data = {
@@ -75,13 +75,12 @@ module.exports = {
             mensagem: "Bem-vindo ao EJS!",
             path: req.url,
         };
-
         const news3 = news.map(objeto => ({
             ...objeto
         })).slice(0, 3);
-
         res.render(navegateRouter(req.params.id), { data, leadership, news3 });
     },
+
     async contactPage(req, res) {
         const data = {
             titulo: "Grupo Boavida - Grupo empresarial: Inovação e gestão por Angola",
@@ -95,7 +94,7 @@ module.exports = {
     },
     companyPage(req, res) {
         const data = {
-            titulo: "Grupo Boavida - Grupo empresarial: Inovação e gestão por Angola",
+            titulo: "Grupo Boavida - Business group: Innovation and management for Angola",
             mensagem: "Bem-vindo ao EJS!",
             path: req.url,
             pathName: req.params.id
@@ -110,4 +109,8 @@ module.exports = {
     sacPage(req, res) {
         res.redirect("https://sac-suporte.onrender.com/");
     }
+
+
+
+
 }
